e34a922eecfe290690f50ecbdfb77b659bb4d7ae
fuldaros <litefros@gmail.com>
2020-01-06 05:53:08 +0300

    fixup

--- base/device_get_repo.sh
+++ base/device_get_repo.sh
@@ -2,7 +2,7 @@ cd build/$rom
 echo "# GET DEVICE TREE"
 git clone $repo_device device/$MAN/$DEVICE
 echo "# GET KERNEL SOURCE"
-git clone $repo_kernel kenel/$MAN/$CPU
+git clone $repo_kernel kernel/$MAN/$CPU
 echo "# GET VENDOR"
 git clone $repo_vendor vendor/$MAN
 cd ../..
