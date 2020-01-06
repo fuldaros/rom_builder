e6475cd68a3ff4149a2f27f8d12e50f1a36d3883
xyzx <litefros@gmail.com>
2020-01-06 04:24:58 +0300

    add repo cache

--- base/android_build.sh
+++ base/android_build.sh
@@ -2,4 +2,5 @@ cd build/$rom
 . build/envsetup.sh
 lunch "$tree_type"_"$DEVICE"-"$2"
 mka -j"$(cat /proc/cpuinfo | grep processor | wc -l)" bacon
-
+cd ../../
+mv ../../repo_cache/$rom/.repo ../../repo_cache/$rom/repo
--- base/android_lineage_pie.sh
+++ base/android_lineage_pie.sh
@@ -1,7 +1,33 @@
+if ! [ -d build/ ]; then
+	mkdir build/
+fi
 cd build/
-mkdir $rom/
+if ! [ -d $rom/ ]; then
+	mkdir $rom/
+else
+	rm -rf $rom
+	mkdir $rom/
+fi
 cd $rom
-repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
+cd build/
+if ! [ -d ../../repo_cache/ ]; then
+	mkdir ../../repo_cache/
+fi
+if ! [ -d ../../repo_cache/$rom ]; then
+	mkdir ../../repo_cache/$rom
+	cd ../../repo_cache/$rom
+	repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
+else
+	if ! [ -d ../../repo_cache/$rom/repo ]; then
+		cd ../../repo_cache/$rom
+		mv repo .repo
+	else
+		cd ../../repo_cache/$rom
+		repo init -u git://github.com/LineageOS/android.git -b lineage-16.0
+	fi
+fi
+
 repo sync -j"$(cat /proc/cpuinfo | grep processor | wc -l)" --force-sync --no-tags --no-clone-bundle
+mv * ../../build/$rom
 tree_type=lineage
 cd ../../
