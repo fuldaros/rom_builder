1a5864cad3c0ccdb5e39db59313b87bde16a607d
xyzx <litefros@gmail.com>
2020-01-12 11:33:46 +0300

    fix typo

--- base/android_build.sh
+++ base/android_build.sh
@@ -1,6 +1,6 @@
-cd build/"$rom"_"$ANDROID"
+cd build/"$rom"-"$ANDROID"
 . build/envsetup.sh
 lunch "$tree_type"_"$DEVICE"-"$2"
 mka -j"$(cat /proc/cpuinfo | grep processor | wc -l)" bacon
 cd ../../
-mv repo_cache/"$rom"_"$ANDROID"/.repo repo_cache/"$rom"_"$ANDROID"/repo
+mv repo_cache/"$rom"-"$ANDROID"/.repo repo_cache/"$rom"-"$ANDROID"/repo
